47f080d7dea0926484c5d08289e773843f64f51e
bharbir <harbirbajwa@hotmail.com>
2024-07-31 18:58:15 -0700

    using variable port number

--- api/app.js
+++ api/app.js
@@ -33,8 +33,8 @@ app.use(session({
     saveUninitialized: true
   }));
 
-app.listen(3000, () => {
-    console.log("Server is running on port http://localhost:3000")
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
 
 module.exports = app;
