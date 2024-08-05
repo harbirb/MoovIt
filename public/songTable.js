e672e888a9e9074e40f7db908e02df42006474ae
bharbir <harbirbajwa@hotmail.com>
2024-08-04 23:23:51 -0700

    typo in referencing song table

--- public/songTable.js
+++ public/songTable.js
@@ -1,5 +1,5 @@
 function populateTable(data) {
-    const tableBody = document.querySelector("#datatable tbody")
+    const tableBody = document.querySelector("#songTable tbody")
     data.forEach(item => {
         const row = document.createElement("tr")
         const title = document.createElement("h2")
