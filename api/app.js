3df6683ff715e1ebf58e4f85c301ec27ec50d9aa
Harbir <121403133+harbirb@users.noreply.github.com>
2025-02-24 00:18:19 -0800

    Revert "remove port from urls"

--- api/app.js
+++ api/app.js
@@ -49,8 +49,7 @@ app.use("/api", authenticate);
 app.use(express.static(path.resolve(__dirname, "../public")));
 app.use(express.json());
 
-app.listen(PORT, () => console.log(`Server is running on ${BASE_URL}`));
-
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 // Authentication Middleware
 function authenticate(req, res, next) {
@@ -235,7 +234,6 @@ app.get("/auth/spotify", (req, res) => {
         scope,
         state,
         redirect_uri: `${BASE_URL}/auth/spotify/callback`,
-        show_dialog: true,
       })
   );
 });
